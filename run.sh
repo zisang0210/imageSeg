1f3ba0135e1f8755b31c85866871382540723c72
zisang <467862440@qq.com>
2018-02-10 10:05:40 +0800

    learning rate selection

--- run.sh
+++ run.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
-i=2
-echo "############" $i "training #################"
-python train_eval.py --checkpoint_path=/home/zisang/Documents/lesson9/vgg16-ckpt/vgg_16.ckpt \
---train_dir=../output/train \
---eval_dir=../output/eval/train \
---dataset_train=./fcn_train.record \
---dataset_val=./fcn_train.record \
---batch_size=16 \
---num_pics=5 \
---max_steps=200 \
---learning_rate=0.0003
 
-echo "############" $i "evaluating #################"
-python eval.py --train_dir=../output/train \
---eval_dir=../output/eval/eval \
---dataset_val=./fcn_val.record \
---num_pics=5
-
+lr=(0.0003 
+  0.0003 0.0003 0.0003 0.0003 
+  0.0001 0.0001 0.0001
+  0.00005 0.00005
+  0.00001)
 # evaluate performance every epoch
-for i in $(seq 3 10)
+for i in $(seq 1 10)
 do
     echo "############" $i "training #################"
   #   python train.py --checkpoint_path=/home/zisang/Documents/lesson9/vgg16-ckpt/vgg_16.ckpt \
@@ -35,7 +23,7 @@ do
   --batch_size=16 \
   --num_pics=5 \
   --max_steps=200 \
-  --learning_rate=0.0001
+  --learning_rate=${lr[$i]}
 
     echo "############" $i "evaluating #################"
     python eval.py --train_dir=../output/train \
@@ -44,31 +32,6 @@ do
   --num_pics=5
 done
 
-for i in $(seq 11 12)
-do
-    echo "############" $i "training #################"
-  #   python train.py --checkpoint_path=/home/zisang/Documents/lesson9/vgg16-ckpt/vgg_16.ckpt \
-  # --train_dir=../output/train \
-  # --dataset_train=./fcn_train.record \
-  # --batch_size=16 \
-  # --max_steps=2
-
-    python train_eval.py --checkpoint_path=/home/zisang/Documents/lesson9/vgg16-ckpt/vgg_16.ckpt \
-  --train_dir=../output/train \
-  --eval_dir=../output/eval/train \
-  --dataset_train=./fcn_train.record \
-  --dataset_val=./fcn_train.record \
-  --batch_size=16 \
-  --num_pics=5 \
-  --max_steps=200 \
-  --learning_rate=0.00005
-
-    echo "############" $i "evaluating #################"
-    python eval.py --train_dir=../output/train \
-  --eval_dir=../output/eval/eval \
-  --dataset_val=./fcn_val.record \
-  --num_pics=5
-done
 # # 设置目录，避免module找不到的问题
 # export PYTHONPATH=$PYTHONPATH:/home/zisang/objDetect:/home/zisang/objDetect/slim:/home/zisang/objDetect/object_detection
 
